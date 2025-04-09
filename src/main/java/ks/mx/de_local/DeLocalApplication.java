173135e9563bf1b6f3503fc845cfaedf2f19669f
Cresciuc Maxim <113927586+KMaxKR@users.noreply.github.com>
2025-04-09 15:36:37 +0300

    main

--- src/main/java/ks/mx/de_local/DeLocalApplication.java
+++ src/main/java/ks/mx/de_local/DeLocalApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeLocalApplication {
-	///
 	public static void main(String[] args) {
 		SpringApplication.run(DeLocalApplication.class, args);
 	}
