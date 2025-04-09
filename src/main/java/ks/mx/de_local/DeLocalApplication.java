7b468e99cd8f9b3fbc6c7d2f3111a90138282dc1
Cresciuc Maxim <113927586+KMaxKR@users.noreply.github.com>
2025-04-09 09:51:13 +0300

    Initial commit

---
+++ src/main/java/ks/mx/de_local/DeLocalApplication.java
@@ -0,0 +1,13 @@
+package ks.mx.de_local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeLocalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ks/mx/de_local/DeLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package ks.mx.de_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
